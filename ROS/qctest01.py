3df6f1542932627cf7eb18ead57fe184369498e1
kengo <naruawaryousuke@gmail.com>
2022-07-14 22:34:24 +0900

    qctest01をpython3に対応

--- ROS/qctest01.py
+++ ROS/qctest01.py
@@ -5,14 +5,14 @@ import struct
 
 def ws_recv():
     data = ws.recv()
-    recv1 = list(struct.unpack_from("B",data[0],0))
-    recv2 = list(struct.unpack_from("B",data[1],0))
-    recv = recv1 + recv2
+    
+    print(data)
+    recv=str(data[0])+str(data[1])
     return recv
 
-ws = create_connection("ws://192.168.1.136:54323")
-print "Open"
-print "Sending command"
+ws = create_connection("ws://192.168.0.108:54323")
+print( "Open")
+print( "Sending command")
 
 command_stp      =  [0xff,0x55,0x01,0x06]
 command_dwn      =  [0xff,0x55,0x04,0x01,0x07,0xc8,0x00]
@@ -43,86 +43,88 @@ command_spLe     =  [0xff,0x55,0x05,0x0b,0x26,0x01,0x7d,0x00]
 
 
 ws.send(command_rbw)
-print "Received '{}'".format(ws_recv())
+print("--------------------")
+
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_fw)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(2)
 
 ws.send(command_fwUH)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(2)
 
 
 ws.send(command_stp)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_up)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_spDo)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_spLe)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_red)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_rw)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(2)
 
 ws.send(command_stp)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 ws.send(command_gln)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 ws.send(command_ble)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 ws.send(command_yel)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 ws.send(command_vio)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 ws.send(command_aqu)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 ws.send(command_ledoff)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 
 
 #ws.send(command_stp)
-#print "Received '{}'".format(ws_recv())
+#print( "Received '{}'".format(ws_recv()))
 #time.sleep(0.5)
 
 ws.send(command_spDo)
-print "Received '{}'".format(ws_recv())
+print( "Received '{}'".format(ws_recv()))
 time.sleep(0.5)
 
 
 
 ws.close()
 
-print "Close"
+print( "Close")
 
